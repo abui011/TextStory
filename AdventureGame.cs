0bf0badafcacffd41dbf9c3f9f342818c61d987b
Alex Bui <alexbui@sorathas-MacBook-Pro.local>
2018-06-29 04:49:56 -0700

    2nd commit

--- AdventureGame.cs
+++ AdventureGame.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,14 +7,31 @@ using UnityEngine.UI;
 public class AdventureGame : MonoBehaviour {
 
     [SerializeField] Text textComponent;
+    [SerializeField] State startingstate;
 
+    State state;
 	// Use this for initialization
 	void Start () {
-        textComponent.text = ("ee");
+        state = startingstate;
+        textComponent.text = state.GetStateStory();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        ManageState();
 	}
+
+    private void ManageState()
+    {
+        var nextStates = state.GetNextStates();
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            state = nextStates[0];
+        }
+        else if(Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            state = nextStates[1];
+        }
+        textComponent.text = state.GetStateStory();
+    }
 }
---
+++ State.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject {
+
+    [TextArea(10,14)][SerializeField] string storyText;
+    [SerializeField] State[] nextstates;
+
+    public State[] GetNextStates(){
+        return nextstates;
+    }
+    public string GetStateStory(){
+        return storyText;
+    }
+}
